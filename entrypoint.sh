b8624a594a031f43b578837e4ced605d380ccb50
gstracqu <giovanni.stracquadanio@ed.ac.uk>
2020-04-28 19:16:53 +0100

    GS: updated label format

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 
 PREFIX=$1
 
-COMMIT_COUNT=$(git rev-list HEAD | wc -l)
-
-echo ::set-output name=release_label::"${PREFIX}${GITHUB_REF#refs/heads/}-v${COMMIT_COUNT}"
+COMMIT_COUNT=$(git rev-list --count HEAD)
+CURRENT_DATE=$(date -u +"%Y.%m.%d")
+LABEL="${PREFIX}${GITHUB_REF#refs/heads/}.${CURRENT_DATE}.${COMMIT_COUNT}"
+echo ::set-output name=release_label::"${LABEL}"
