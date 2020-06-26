67b67fd8da6f6cf95bb6098e267c2ed62b6ed4dc
gstracqu <giovanni.stracquadanio@ed.ac.uk>
2020-06-26 22:17:46 +0100

    GS: updated progressive number

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,6 @@ set -e
 
 PREFIX=$1
 
-COMMIT_COUNT=$(git rev-list --count HEAD)
 CURRENT_DATE=$(date -u +"%Y.%m.%d")
-LABEL="${PREFIX}${GITHUB_REF#refs/heads/}.${CURRENT_DATE}.${COMMIT_COUNT}"
+LABEL="${PREFIX}${GITHUB_REF#refs/heads/}.${CURRENT_DATE}.${GITHUB_RUN_NUMBER}"
 echo ::set-output name=release_label::"${LABEL}"
