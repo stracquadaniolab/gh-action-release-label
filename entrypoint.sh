b4bba03f633b8bf581264b27351e8c0cdfa6ab6f
Giovanni Stracquadanio <giovanni.stracquadanio@ed.ac.uk>
2020-10-08 14:20:31 +0100

    added release message commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@ set -e
 
 PREFIX=$1
 
-CURRENT_DATE=$(date -u +"%Y.%m.%d")
-LABEL="${PREFIX}${GITHUB_REF#refs/heads/}.${CURRENT_DATE}.${GITHUB_RUN_NUMBER}"
+CURRENT_DATE=$(date -u +"%Y-%m-%d")
+MESSAGE=$(git log --format=%B -n 1 ${GITHUB_SHA})
+LABEL="${PREFIX}-${CURRENT_DATE}-r${GITHUB_RUN_NUMBER}"
+
 echo ::set-output name=release_label::"${LABEL}"
+echo ::set-output name=release_message::"${MESSAGE}"
